6cec0f3326cd12a8465dc63f4acc350b1d82e1fb
mrssx10 <159055097+mrssx10@users.noreply.github.com>
2024-08-19 15:20:44 -0300

    Add files via upload

---
+++ dump.sqlite (2).sql
@@ -0,0 +1,17 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE jogos_esportivos (
+    esporte VARCHAR(50),
+    equipe_casa VARCHAR(50),
+    equipe_visitante VARCHAR(50),
+    pontuacao_casa INT,
+    pontuacao_visitante INT,
+    data_jogo DATE
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
